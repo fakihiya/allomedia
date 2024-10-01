57933b5a5d1cb3a717a2962a2d1a6639f957852b
fakihiya <yasinfakihi21@gmail.com>
2024-10-01 09:36:31 +0100

    add reset password, forget password, and OTP code functionality

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,16 @@
 // routes/api.js
 const express = require('express');
 const router = express.Router();
-const {register,login} = require ('../controllers/UserController')
-// const { login } = require('../controllers/UserController'); // Make sure the path is correct
+const { register, login, verifyEmail, verifyOTP, forgotPassword, resetPassword,logout } = require('../controllers/UserController');
 
+router.post('/users/register', register);
+router.post('/users/login', login);
+router.get('/users/verify/:token', verifyEmail);
+router.post('/users/verify-otp', verifyOTP);
 
-router.post('/users/register', register)
-router.post('/users/login',login)
+router.post('/forgot-password', forgotPassword);
+
+router.post('/reset-password/:token', resetPassword);
+router.post('/users/logout', logout);
 
 module.exports = router;
