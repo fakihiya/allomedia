fee82d10a64342fbf5505c09943a7fa128556343
fakihiya <yasinfakihi21@gmail.com>
2024-10-06 10:23:14 +0100

    install CORS

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const cors = require('cors');
 require("dotenv").config();
 
 const connectDB = require('./config/database'); // Import the connection function
@@ -9,9 +9,17 @@ const apiRoutes = require("./routes/api");
 const webRoutes = require("./routes/web");
 
 const app = express();
+// Use CORS middleware
+// app.use(cors({
+//     origin: 'http://localhost:3000', // Change this to your frontend URL
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow the necessary HTTP methods
+//     credentials: true // If you need to include credentials like cookies
+// }));
 
 // middlewares
 app.use(express.json());
+app.use(cors());
+
 app.use(express.static("public"));
 app.use("/", webRoutes);
 app.use("/api", apiRoutes);
