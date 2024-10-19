4bb5c04d36c132f17643166767150125ac6b9ee4
fakihiya <yasinfakihi21@gmail.com>
2024-10-19 15:41:40 +0100

    Update UserController.js

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,4 @@
-const User = require('../models/user');
+const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { sendverificationemail , sendEmail} = require('../services/emailService');
